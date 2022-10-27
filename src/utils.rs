47bc1c1c80510eb0383c154fa6764b1a51377cbe
Crystal Kate <crystalnyan@icloud.com>
2022-10-27 15:40:20 +0500

    add utils functions

---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+pub  fn show_default_menu() {
+	println!(
+		"Welcome to meow library!~\n\
+        What you wanna do?\n\
+        1. Enter library as a customer\n\
+        2. Enter as a librarian\n\
+        3. Leave");
+}
