d6c55f858677f22102bd62ec6ce98eb336739a80
Crystal Kate <crystalnyan@icloud.com>
2022-10-27 17:28:37 +0500

    small fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::io;
+
 pub mod types;
 pub mod utils;
 
 fn main() {
-    utils::show_default_menu();
+  //menu with entering/leaving library
+  //1-enter as a user 2-enter as a librarian 3-exit
+  utils::show_default_menu();
+
+  let mut choice = utils::get_choice();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,18 +1,18 @@
 #[derive(Debug)]
 struct User {
-	pub name: String,
-	pub role: Roles,
-	pub book_taken: Book
+  pub name: String,
+  pub role: Roles,
+  pub book_taken: Book,
 }
 
 #[derive(Debug)]
 pub struct Book {
-	pub title: String,
-	pub author: String,
+  pub title: String,
+  pub author: String,
 }
 
 #[derive(Debug)]
-enum Roles{
-	Customer,
-	Librarian
+enum Roles {
+  Customer,
+  Librarian,
 }
