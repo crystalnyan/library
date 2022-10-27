c23698fd161230ec71b6d45a1b7b9ca99cc2f5dc
Crystal Kate <crystalnyan@icloud.com>
2022-10-27 17:27:50 +0500

    add fn to get user choice

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,25 @@
+use std::io;
+
 pub fn show_default_menu() {
-	println!(
-		"Welcome to meow library!~\n\
+  println!(
+	"Welcome to meow library!~\n\
         What you wanna do?\n\
         1. Enter library as a customer\n\
         2. Enter as a librarian\n\
         3. Leave");
+}
+
+pub fn get_choice() -> Option<u8> {
+  let mut choice = String::new();
+
+  io::stdin()
+	  .read_line(&mut choice)
+	  .expect("Can't read the choice TT");
+
+  let choice: Option<u8> = match choice.trim().parse() {
+	Ok(num) => Some(num),
+	Err(_) => None
+  };
+
+  choice
 }
