5ff814885a7a8f3c66d4a2f079a3160a912064f9
Crystal Kate <crystalnyan@icloud.com>
2023-01-07 11:23:26 +0500

    add getters and from_tuple function

--- src/types/book.rs
+++ src/types/book.rs
@@ -12,13 +12,20 @@ impl Book {
             quantity
         }
     }
+
+    pub fn get_title (&self) -> String {self.title.clone()}
+    pub fn get_author (&self) -> String {self.author.clone()}
+    pub fn get_quantity (&self) -> String {self.quantity.to_string()}
+
+    pub fn from_tuple(title: &String, author: &String, quantity: String) -> Book {
+        let quantity = quantity.trim().parse().unwrap();
+        Book::new(title.clone(), author.clone(), quantity)
+    }
 }
 
 impl ToString for Book {
     fn to_string(&self) -> String {
-        let record =
-            "\n".to_string()
-            + &self.title + &(self.author) + &(self.quantity.to_string());
+        let record = self.title.clone() + &(self.author) + &(self.quantity.to_string()) + "\n";
         String::from(record)
     }
 }
